6d08082d406df2ddc0c2f810410a5992b9723966
RAMZIL <ramzilhuda1986@gmail.com>
2020-08-02 09:09:01 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Image Widget"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(3),
+            child: Image(
+              image: AssetImage("images/flutter.png"),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
